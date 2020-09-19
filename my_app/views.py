781d460eaacc1bdca5e5c1c1a377f5821c1c7f83
Mradul Surana <msurana@wisc.edu>
2020-09-19 12:51:15 -0500

    added to composition routes

--- my_app/views.py
+++ my_app/views.py
@@ -5,3 +5,7 @@ import requests
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/composition")
+def composition():
+    return render_template("composition.html")
